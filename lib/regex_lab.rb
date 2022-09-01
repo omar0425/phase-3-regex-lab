7f5508aee41f6146ad8df63bfdb01cf454d654ea
omar0425 <startfresh87@gmail.com>
2022-09-01 10:32:58 -0400

    commit tree b2515128eaff763c71b0103a18a03c201cb167db

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,19 @@
+require "pry"
 def starts_with_a_vowel?(word)
-
+  # binding.pry
+/^[aeiouAEIOU]/.match?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+/^[A-Z].+[\.!?]$/.match?(text)
 end
 
 def valid_phone_number?(phone)
